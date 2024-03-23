171bf181895cf44a47da6fe044d4f45f143df94d
Dependra Kumar <dependragitcreator@gmail.com>
2024-03-23 16:39:47 +0530

    utiles code disscussion

---
+++ src/app.js
@@ -0,0 +1,25 @@
+//express here
+import express from 'express'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+const app = express();
+
+
+//ydi hume middleware use karna hai to app.use() method se hi middlewware ko use karte hai & configuration bhi isi tarah se karte hai
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//json format mein limited data send karna because ydi user unlimited data send karega to server cresh ho jayega
+app.use(express.json({limit: "16kb"}))
+//url mein jo spaces hote hai uske lia kyuki space ko %20 se replace karte hai
+//multiple url ko encode karne ke lia "extended: true" use karte hai
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+//server par ydi koi file/photo rakhna hai to "static" configuration use karte hai esse "public assests" bhi khte hai
+app.use(express.static("public"))
+//cookies ko access karna & cookies ko set karna , user browser mein securely server side se => basically "CRUD" operation on cookies
+app.use(cookieParser())
+export default app;
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: '../env'})     //inconsistency ki vajah se ese use nhi kar rhe hai
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import app from './app.js';
 
 
 dotenv.config({
@@ -9,7 +10,21 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(() => {
+
+    //ye listen se uppar eslia likha gya hai because ydi port par listen karne mein koi problem aayi to , listen karne se phle error aayi hai ya nhi ye check karna padega
+    app.on("error", (error) => {
+        console.log("your app is not able to talk with database !!! error: ", error);
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`app is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Database connection failed", error);
+})
 
 
 
@@ -32,6 +47,7 @@ const app = express();
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 
+        //ydi port par listen karne mein koi error hai uske lia hai ye app.on("error", (error) => {console.log(error)})
         app.on("error", (error) => {
             console.log("Your app is not able to talk with database, error:", error);
             throw error;

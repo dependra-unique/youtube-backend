7372de87aef21edfa20040709ab559abf9bb4cca
Dependra Kumar <dependragitcreator@gmail.com>
2024-03-24 13:51:04 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
-const userSchema = mongoose.Schema(
+//database schema using mongoose 
+const userSchema = new mongoose.Schema(
     {
         username: {
             type: String,
@@ -17,7 +20,7 @@ const userSchema = mongoose.Schema(
             unique: true,
             trim: true 
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -43,12 +46,59 @@ const userSchema = mongoose.Schema(
         },
          refreshToken: {
             type: String
-         }
+         }  
     },
     {
         timestamps: true
     }
 )
 
+//password encrytion
+//database ke userSchema mein ydi password save kar rhe ho to ,password save karne se phle password ko encrypt karo and then save karo
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+
+//User model ko ydi hum kisi or file mein import karana chahte hai to us file mein bhi password encryted hona chahiye eslia compare kar lo import karne se phle hi
+userSchema.methods.isPasswordCorrect = async function (password){
+
+    return await bcrypt.compare(password, this.password)
+}
+
+
+
+//jwt uses => json web token
+//access token
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+//refresh token
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);

7fa733aa9871c02e3dded7df0dbf78b79f11cc24
Dependra Kumar <dependragitcreator@gmail.com>
2024-04-09 12:00:19 +0530

    add updateComment method endpoint

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -60,6 +60,42 @@ const addComment = asyncHandler( async (req, res) => {
 
 const updateComment = asyncHandler( async (req, res) => {
     //TODO: update comment on video
+    
+    //get comment by comment id,,, database se because jisne comment kia hai usi user ko allow karege comment update karne ke lia
+    const {commentId} = req.params;
+    const comment = Comment.findById(commentId)
+
+    //user se content le lo comment update karne ke lia
+    const {content} =  req.body
+
+    if(content === ""){
+        throw new ApiError(404, "content is required");
+    }
+
+    if(!comment){
+        throw new ApiError(400, "comment id not found")
+    }
+
+    //check karo jo user comment kar raha hai vo owner hai ya nhi
+    if(comment?.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(404, "Only owner can update the comment")
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        comment?._id,
+        {
+            $set: {
+                content
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    return res
+    .status(200)
+    .json(200, updatedComment, "Comment update  successfully")
 })
 
 const deleteComment = asyncHandler( async (req, res) => {
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import {addComment} from '../controllers/comment.controller.js'
+import {addComment, updateComment} from '../controllers/comment.controller.js'
 
 const router = Router();
 
 router.use(verifyJWT)    //verfy user hi comment kare
 router.route("/addComment").post(addComment)
-
+router.route("/updateComment").post(updateComment)
 
 
 export default router
